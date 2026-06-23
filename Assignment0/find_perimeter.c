cdc67b46d9a980c9bb33e2f122faec1aa146f26c
AJAYBIRADARA <biradara311@gmail.com>
2026-06-23 11:15:30 -0400

    removing extra folder

--- Assignment0/find_perimeter.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main(){
-	int Perimeter,L=10,W=5;
-	Perimeter=(L+W)*2;
-	printf("perimeter is :%d",	Perimeter);
-	getch();
-}
--- Assignment0/tempurature_celsius_to_fahreheit.c
+++
@@ -1,9 +0,0 @@
-// 3 Write a C program to convert temperature from Celsius to Fahrenheit using the formula: F = (C *9/5) + 32 
-#include<stdio.h>
-#include<conio.h>   //  optional ! my system can't hold output screen that why i use it 
-void main(){
-	float Fahrenheit, Celsius=30;
-	Fahrenheit=(Celsius *9/5) + 32;
-	printf("Fahrenheit is %f",	Fahrenheit);
-	getch();  //  optional 	
-}
